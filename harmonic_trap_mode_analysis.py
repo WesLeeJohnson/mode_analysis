959cc050358c5b9f1abb7e82a2c977a71e987a96
WesLeeJohnson <wejo44@colorado.edu>
2024-05-31 15:56:26 -0600

    This file will contain the harmonic trap class, which will be used to solve the eigenvalue problem for a harmonic trap

---
+++ harmonic_trap_mode_analysis.py
@@ -0,0 +1,9 @@
+"""
+Author:     Wes Johnson
+Date:       May 31st, 2024 
+Purpose:    Perform the normal mode analysis for an ion crystal in a harmonic trap. 
+How to run: 
+            import harmonic_trap_mode_analysis as htma
+            htma_obj = htma.HarmonicTrapModeAnalysis(<parameters>)
+            htma_obj.run()
+"""
